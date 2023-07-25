d7367240fcec12be53457672d1df56584aa92be3
Dmitriy <basstiliomusic@gmail.com>
2023-07-25 09:50:14 +0300

    Это наш первый комит

---
+++ Function.js
@@ -0,0 +1,17 @@
+    var Lad = 2;
+   
+    // объект
+    console.log (Lad);
+
+    let persone = {
+       name:'jonh',
+       age: 25,
+       isMarried: false
+
+    };
+
+    console.log(persone.age);
+
+    // массив
+    let arr = ['qwe','qwd'];
+    console.log(arr[0]);
---
+++ Untitled-1.js
@@ -0,0 +1,8 @@
+var leftBorderWidth = 5;
+let second = 2;
+const pi = 3.14;
+console.log (leftBorderWidth)
+
+
+
+console.log (4/2)
